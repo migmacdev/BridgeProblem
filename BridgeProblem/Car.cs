e309b1c3f332ea91aabb83d97a24f5ec98a4432a
Miguel Angel <iscmiguelmaciel@outlook.com>
2017-01-20 20:56:37 -0600

    Add Car Producer class,to generate cars from a Thread, Add Cross function to Car class

--- BridgeProblem/Car.cs
+++ BridgeProblem/Car.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace BridgeProblem
 {
@@ -20,8 +20,9 @@ namespace BridgeProblem
 
         //Car properties
         private int id;
-        private string StartPoint;
-        private string EndPoint;
+        private string startPoint;
+        private string endPoint;
+        private Direction direction;
 
         public Car()
         {
@@ -31,21 +32,35 @@ namespace BridgeProblem
         public Car(int id, Direction dir)
         {
             this.id = id;
+            this.direction = dir;
             if (dir == Direction.LeftToRight)
             {
-                StartPoint = "Left";
-                EndPoint = "Right";
+                startPoint = "Left";
+                endPoint = "Right";
             }
             else
             {
-                StartPoint = "Righ";
-                EndPoint = "Left";
+                startPoint = "Righ";
+                endPoint = "Left";
             }
         }
 
+        public Direction GetDirection() { return this.direction; }
+
         public string ToString()
         {
-            return "Car id : " + this.id + ", From : " + StartPoint + ", To : " + EndPoint;
+            return "Car id : " + this.id + ", From : " + startPoint + ", To : " + endPoint;
+        }
+
+        //Cross method,print the status of the car in the bridge
+        public void Cross()
+        {
+            Console.WriteLine("Vehiculo " + id + "entra por " + startPoint);
+            //Simulate Travel
+            Thread.Sleep (100);
+			Console.WriteLine ("Vehiculo " + id + " pasa puente");
+			Thread.Sleep (100);
+			Console.WriteLine("Vehiculo " + id + " sale por " + endPoint);
         }
     }
 }
---
+++ BridgeProblem/CarProducer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BridgeProblem
+{
+
+    public class CarProducer
+    {
+        int rightCars;              //Number of cars with right-to-left direction
+        int leftCars;               //Number of cars with left-to-right direction
+        int currentId = 0;          //Id used to name new cars, increments after creation
+        int productionSpeed = 1000; //Time to spawn a new car
+
+        Random rnd;
+
+        //Setting up properties
+        public CarProducer(int numOfRightCars, int numOfLeftCars)
+        {
+            this.rightCars = numOfRightCars;
+            this.leftCars = numOfLeftCars;
+            rnd = new Random();
+        }
+
+        //Creates a car with a random direction if available, otherwise returns null
+        public Car produce()
+        {
+            Car c = null;
+
+            if (rightCars == 0 && leftCars == 0)
+            {
+                return null;
+            }
+
+            int rndNum = rnd.Next(0, 2);
+
+            //Simulates delay
+            Thread.Sleep(rnd.Next(productionSpeed));
+
+            //Generates cars if there are space available in both sides
+            if (rndNum == 0 && rightCars > 0)
+            {
+                c = new Car(currentId, Car.Direction.RightToLeft);
+                rightCars--;
+            }
+            else if (rndNum == 1 && leftCars > 0)
+            {
+                c = new Car(currentId, Car.Direction.LeftToRight);
+                leftCars--;
+            }
+            
+            //Generates a different car if the option is not available
+            else if (rndNum == 1 && leftCars == 0 && rightCars > 0)
+            {
+                c = new Car(currentId, Car.Direction.RightToLeft);
+                rightCars--;
+            }
+            else if (rndNum == 0 && rightCars == 0 && leftCars > 0)
+            {
+                c = new Car(currentId, Car.Direction.LeftToRight);
+                leftCars--;
+            }
+
+            currentId++;
+            return c;
+        }
+
+    }
+
+}
