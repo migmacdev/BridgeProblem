724bb17a2e414f0c284344a4469f1a763c599246
Miguel Angel <iscmiguelmaciel@outlook.com>
2017-01-21 12:53:46 -0600

    Remove Unnecessary usings

--- BridgeProblem/Car.cs
+++ BridgeProblem/Car.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 
 namespace BridgeProblem
 {
-    
+
     /// <summary>
     /// Object representing the car and it's properties
     /// </summary>
